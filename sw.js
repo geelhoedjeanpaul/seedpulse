0bff952a468ec47b102080ec39202dff27ca8881
Jean-Paul Geelhoed <jean-paulgeelhoed@Jean-Pauls-MacBook-Air.local>
2026-04-16 08:59:58 +0200

    Initial SeedPulse PWA

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'seedpulse-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Cache-first for app shell; network-first for external resources
+  if (e.request.url.startsWith(self.location.origin)) {
+    e.respondWith(
+      caches.match(e.request).then(cached => {
+        const network = fetch(e.request).then(res => {
+          caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+          return res;
+        });
+        return cached || network;
+      })
+    );
+  }
+});
